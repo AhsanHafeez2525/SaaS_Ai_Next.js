78f61859ac310008f002d76dedcf0bda5a946a3c
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2024-10-27 23:20:13 +0500

    folder setup

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      <p className="text-6xl text-green-400">Dashboard Page (Protected)</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
